bb2916e8c80cd4a5f037f0aac0fe009cf7269113
bigmaggi <stuffstuff@nightmare.life>
2023-07-07 21:05:32 +0200

    first refactor with an error on linux

--- main.py
+++ main.py
@@ -6,27 +6,15 @@ console = Console()
 todos = {"eat apple": "No", "put banana somewhere": "Yes", "Fuck Orange": "No", "Yeet grape": "Yes"}
 my_list_keys = list(todos.keys())
 
+# Initialize the table
 table = Table(show_header=True, header_style="bold", title="Todos")
 table.add_column("Index")
 table.add_column("Todo")
 table.add_column("Done?")
 
-# Add table rows
-for index, key in enumerate(my_list_keys):
-    table.add_row(str(index), key, todos[key])
-
-console.print(table)
-
-# Store the currently selected row index
-selected_row = 0
-
 # Function to update the table display
 # make text strikethrough when value == Yes
-def update_table():
-    # Clear the console
-    console.clear()
-
-    # Create a new table with updated selection
+def update_table(selected_row):
     new_table = Table(show_header=True, header_style="bold", title="Todos")
     new_table.add_column("Index")
     new_table.add_column("Todo")
@@ -49,69 +37,77 @@ def update_table():
             else:
                 new_table.add_row(str(index), key, todos[key])
 
-    console.print(new_table)
+    return new_table
 
 # Function to change the "Done?" status of the selected todo
-def change_text():
+def change_text(selected_row):
     todo = my_list_keys[selected_row]
     current_status = todos[todo]
     if current_status == "No":
         todos[todo] = "Yes"
     elif current_status == "Yes":
         todos[todo] = "No"
-    update_table()
 
 # Function to add a new todo to the table
 def add_todo():
     new_todo = input("Enter the new todo: ")
     todos[new_todo] = "No"
     my_list_keys.append(new_todo)  # Add the new todo key to the list of keys
-    update_table()
 
 # Function to delete the selected todo from the table
 def delete_todo():
     todo = my_list_keys[selected_row]
     del todos[todo]
     my_list_keys.remove(todo)  # Remove the todo key from the list of keys
-    update_table()
 
-# write a function to save the todos to a file
+# Function to save the todos to a file
 def save_todos():
     with open("todos.txt", "w") as f:
         for key, value in todos.items():
             f.write(f"{key},{value}\n")
 
-# write a function to load the todos from a file
+# Function to load the todos from a file
 def load_todos():
-    with open("todos.txt", "r") as f:
-        for line in f:
-            key, value = line.strip().split(",")
-            todos[key] = value
-            my_list_keys.append(key)
+    try:
+        with open("todos.txt", "r") as f:
+            for line in f:
+                key, value = line.strip().split(",")
+                todos[key] = value
+                my_list_keys.append(key)
+    except FileNotFoundError:
+        print("No todos.txt file found. Starting with no todos.")
 
-# try to load the todos from a file
-try:
+# Main function
+def main():
+    # Load the todos from a file
     load_todos()
-except FileNotFoundError:
-    print("No todos.txt file found. Starting with no todos.")
 
-# Main loop to handle navigation and other actions
-while True:
-    key = readchar.readkey()
+    # Print the initial table
+    selected_row = 0
+    current_table = update_table(selected_row)
+    console.print(current_table)
 
-    if key == "j":
-        selected_row = min(selected_row + 1, len(my_list_keys) - 1)
-        update_table()
-    elif key == "k":
-        selected_row = max(selected_row - 1, 0)
-        update_table()
-    elif key == "c":
-        change_text()
-    elif key == "a":
-        add_todo()
-    elif key == "d":
-        delete_todo()
-    elif key == "s":
-        save_todos()
-    elif key == "q":
-        break
+    # Main loop to handle navigation and other actions
+    while True:
+        key = readchar.readkey()
+
+        if key == "j":
+            selected_row = min(selected_row + 1, len(my_list_keys) - 1)
+        elif key == "k":
+            selected_row = max(selected_row - 1, 0)
+        elif key == "c":
+            change_text(selected_row)
+        elif key == "a":
+            add_todo()
+        elif key == "d":
+            delete_todo()
+        elif key == "s":
+            save_todos()
+        elif key == "q":
+            break
+
+        current_table = update_table(selected_row)
+        console.print(current_table)
+
+if __name__ == "__main__":
+    main()
