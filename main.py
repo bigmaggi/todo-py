c968356c3dc77f77ba8e747e09a4e04e66f0fe16
bigmaggi <stuffstuff@nightmare.life>
2023-07-13 17:46:02 +0200

    works as expected now

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from rich.console import Console
 from rich.table import Table
 import readchar
+import os
+
+# Clear the console
+os.system('cls' if os.name == 'nt' else 'clear')
 
 console = Console()
 todos = {"eat apple": "No", "put banana somewhere": "Yes", "Fuck Orange": "No", "Yeet grape": "Yes"}
@@ -41,12 +45,33 @@ def update_table(selected_row):
 
 # Function to change the "Done?" status of the selected todo
 def change_text(selected_row):
-    todo = my_list_keys[selected_row]
-    current_status = todos[todo]
-    if current_status == "No":
-        todos[todo] = "Yes"
-    elif current_status == "Yes":
-        todos[todo] = "No"
+    # Get the key of the selected row
+    key = my_list_keys[selected_row]
+
+    # new value equals to the opposite of the current value
+    new_value = "Yes" if todos[key] == "No" else "No"
+
+    # Update the todos dictionary with the new value
+    todos[key] = new_value
+
+    # Update the table with the new values
+    current_table = update_table(selected_row)
+
+    # Print the updated table
+    console.clear()
+    console.print(current_table)
+
+# Move the current row down
+def move_down(current_row):
+    if current_row < len(my_list_keys) - 1:
+        current_row += 1
+    return current_row
+
+# Move the current row up
+def move_up(current_row):
+    if current_row > 0:
+        current_row -= 1
+    return current_row
 
 # Function to add a new todo to the table
 def add_todo():
@@ -55,10 +80,13 @@ def add_todo():
     my_list_keys.append(new_todo)  # Add the new todo key to the list of keys
 
 # Function to delete the selected todo from the table
-def delete_todo():
+def delete_todo(selected_row):
+    # Get the key of the selected row
     todo = my_list_keys[selected_row]
+
+    # Remove the todo from the todos dictionary and list of keys
     del todos[todo]
-    my_list_keys.remove(todo)  # Remove the todo key from the list of keys
+    my_list_keys.remove(todo)
 
 # Function to save the todos to a file
 def save_todos():
@@ -68,46 +96,84 @@ def save_todos():
 
 # Function to load the todos from a file
 def load_todos():
-    try:
+    global todos, my_list_keys
+
+    # Check if the file exists
+    if os.path.exists("todos.txt"):
+        # Open the file for reading
         with open("todos.txt", "r") as f:
-            for line in f:
-                key, value = line.strip().split(",")
-                todos[key] = value
-                my_list_keys.append(key)
-    except FileNotFoundError:
-        print("No todos.txt file found. Starting with no todos.")
+            # Read the contents of the file
+            file_contents = f.read()
+
+            # Split the contents into lines
+            lines = file_contents.splitlines()
+
+            # Parse the lines into a dictionary
+            todos = {}
+            for line in lines:
+                key, value = line.split(",")
+                todos[key.strip()] = value.strip()
+
+            # Update the list of keys
+            my_list_keys = list(todos.keys())
+    else:
+        # If the file does not exist, use the default todos
+        todos = {"eat apple": "No", "put banana somewhere": "Yes", "Fuck Orange": "No", "Yeet grape": "Yes"}
+        my_list_keys = list(todos.keys())
 
 # Main function
 def main():
     # Load the todos from a file
     load_todos()
 
+    # Set the initial current row to 0
+    current_row = 0
+
     # Print the initial table
-    selected_row = 0
-    current_table = update_table(selected_row)
+    current_table = update_table(current_row)
     console.print(current_table)
 
-    # Main loop to handle navigation and other actions
+    # Enter the main loop
     while True:
-        key = readchar.readkey()
+        # Get the user input
+        key_press = readchar.readkey()
 
-        if key == "j":
-            selected_row = min(selected_row + 1, len(my_list_keys) - 1)
-        elif key == "k":
-            selected_row = max(selected_row - 1, 0)
-        elif key == "c":
-            change_text(selected_row)
-        elif key == "a":
+        # Handle the user input
+        if key_press == "j":
+            current_row = move_down(current_row)
+            current_table = update_table(current_row)
+            console.clear()
+            console.print(current_table)
+        elif key_press == "k":
+            current_row = move_up(current_row)
+            current_table = update_table(current_row)
+            console.clear()
+            console.print(current_table)
+        elif key_press == "c":
+            change_text(current_row)
+        elif key_press == "a":
             add_todo()
-        elif key == "d":
-            delete_todo()
-        elif key == "s":
+            current_row = len(my_list_keys) - 1
+            current_table = update_table(current_row)
+            console.clear()
+            console.print(current_table)
+        elif key_press == "d":
+            delete_todo(current_row)
+            if current_row > len(my_list_keys) - 1:
+                current_row = len(my_list_keys) - 1
+            current_table = update_table(current_row)
+            console.clear()
+            console.print(current_table)
+        elif key_press == "s":
             save_todos()
-        elif key == "q":
+        elif key_press == "q":
             break
-
-        current_table = update_table(selected_row)
-        console.print(current_table)
+#
+#        if new_row != selected_row:
+#            selected_row = new_row
+#            current_table = update_table(selected_row)
+#            console.clear()
+#            console.print(current_table)
 
 if __name__ == "__main__":
     main()
